3c521c6adc583553460db01acfe1d4a734acd6e1
budarov <budarov@haulmont.com>
2023-01-24 17:15:57 +0400

    disable sorting for Owner columns that backend endpoint cannot sort

--- src/admin/owner/OwnerList.tsx
+++ src/admin/owner/OwnerList.tsx
@@ -4,13 +4,13 @@ export const OwnerList = () => {
     return (
         <List>
             <Datagrid rowClick="show">
-                <TextField source="id" />
+                <TextField source="id" sortable={false}/>
                 <TextField source="firstName" />
                 <TextField source="lastName" />
-                <TextField source="address" />
+                <TextField source="address" sortable={false}/>
                 <TextField source="city" />
-                <TextField source="email" />
-                <TextField source="telephone" />
+                <TextField source="email" sortable={false}/>
+                <TextField source="telephone" sortable={false} />
                 <EditButton />
             </Datagrid>
         </List>
