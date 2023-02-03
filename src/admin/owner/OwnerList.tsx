70e41ae84e19b69bad01d8dd695d6ad9832de7fc
Vadim Basko <vadim.p.basko@gmail.com>
2023-02-03 13:29:34 +0400

    custom query - MUI version

--- src/admin/owner/OwnerList.tsx
+++ src/admin/owner/OwnerList.tsx
@@ -1,23 +1,63 @@
-import {Datagrid, EditButton, List, TextField, TextInput} from "react-admin";
+import {Create, List, SimpleForm, TextInput} from "react-admin";
 
-const postFilters = [
-    <TextInput label="First name" source="firstName"/>,
-    <TextInput label="Last name" source="lastName"/>,
+
+import {ApolloClient, InMemoryCache, ApolloProvider, gql, from} from '@apollo/client';
+import {httpLink} from "../links/httpLink";
+import {useQuery} from "react-query";
+import {DataGrid, GridColDef} from "@mui/x-data-grid";
+import * as React from "react";
+
+const client = new ApolloClient({
+    cache: new InMemoryCache(),
+    link: httpLink
+});
+
+const OWNER_LIST_BY_NAMES_FILTER_OFFSET_PAGE_SORTED = gql(`
+  query Get_Owner_List_With_Filter_Page_Sort($filter: OwnerFilterInput, $page: OffsetPageInput, $sort: [OwnerOrderByInput]) {
+  ownerListByNamesFilterOffsetPageSorted(filter: $filter, page: $page, sort: $sort) {
+    content {
+      id
+      firstName
+      lastName
+      city
+      address
+      telephone
+      email
+    }
+    totalElements
+  }
+}
+`);
+
+const columns: GridColDef[] = [
+    {field: 'id', headerName: 'ID'},
+    {field: 'firstName', headerName: 'First Name'},
+    {field: 'lastName', headerName: 'Last Name'},
 ];
 
 export const OwnerList = () => {
+
+    const {data} = useQuery(
+        ['OwnerDTO', 'getListCustom'],
+        () => {
+            return client.query({
+                query: OWNER_LIST_BY_NAMES_FILTER_OFFSET_PAGE_SORTED
+            }).then(result => {
+                return {data: result.data.ownerListByNamesFilterOffsetPageSorted.content};
+            })
+        }
+    );
+
     return (
-        <List filters={postFilters}>
-            <Datagrid rowClick="show">
-                <TextField source="id" sortable={false}/>
-                <TextField source="firstName" />
-                <TextField source="lastName" />
-                <TextField source="address" sortable={false}/>
-                <TextField source="city" />
-                <TextField source="email" sortable={false}/>
-                <TextField source="telephone" sortable={false} />
-                <EditButton />
-            </Datagrid>
+        <List>
+            <div style={{height: 600, width: '100%'}}>
+                <DataGrid
+                    rows={data?.data ?? []}
+                    columns={columns}
+                    pageSize={10}
+                    checkboxSelection
+                />
+            </div>
         </List>
     );
 };
